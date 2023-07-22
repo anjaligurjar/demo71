d1fc9d5251d36690d16cf6ea4b44e7a5323aa68a
Anjali  Gurjar <anjaligurjar861@gmail.com>
2023-07-22 08:35:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+import RegistrationForm from "./Pages/RegistrationForm"
+import LoginForm from "./Pages/LoginForm";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './App.css';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+
+import Header from "../demo71/Component/Headers";
+import Footer from "../demo71/Component/Footer";
+import Navbarmenu from "../demo71/Component/Navbarmenu";
+import Services from './Pages/Services';
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div class="flex-container"></div>
+     
+
+      
+      
+      <Header />
+      <Router>
+
+        <Navbarmenu />
+        <Routes>
+          <Route path='/Home' element={< Home />}></Route>
+          <Route path='/about' element={< About />}></Route>
+          <Route path='/contact' element={< Contact />}></Route>
+          <Route path='/LoginForm' element={< LoginForm />}></Route>
+          <Route path='/RegistrationForm' element={< RegistrationForm />}></Route>
+          <Route path='/Services' element={< Services/>}></Route>
+
+        </Routes>
+      </Router>
+
+      <Footer />
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ Services.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <div>Services
+ <h1>services feature here </h1>
+ </div>
+  )
+}
+
+export default Services
